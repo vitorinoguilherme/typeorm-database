3eb6f1bb653d36af9aa876af672f834b8d2d75d2
Guilherme Vitorino <vitorino.guilherme@aluno.ifsp.edu.br>
2021-08-31 17:44:24 -0300

    :sparkles: challenge finalized

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,23 @@
-// import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import { getCustomRepository, TransactionRepository } from 'typeorm';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
